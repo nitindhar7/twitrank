22775f7429f32f8de6372aad3609c426bd1a8db4
Nitin Dhar <nitindhar7@yahoo.com>
2011-04-12 05:14:28 -0400

    removed twitter config file and db configs from git repo

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -7,13 +7,6 @@ class SearchesController < ApplicationController
     
     Search.create(:query => params[:query])
     
-    Twitter.configure do |config|
-      config.consumer_key = "VPOnFXqFJZ394HLSs4MnA"
-      config.consumer_secret = "VOHk80F7NvipnRaJSWiRYIROgbLjmCpzqUpnADDDfBY"
-      config.oauth_token = "94414262-Hg4KmGot4IILiPGOKkzEt2R5T16WFM927Ev6uPIiR"
-      config.oauth_token_secret = "RwlgjIK0VODW7pF0hCTbeUB5wdd7PmEFXm5jbmOTA"
-    end
-    
     @query_tweets = []
     @client = Twitter::Client.new
     Twitter::Search.new.containing( params[:query] ).no_retweets.lang("en").per_page( 5 ).each do |tweet|
