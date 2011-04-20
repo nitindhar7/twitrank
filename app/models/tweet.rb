d2c7f07a93a918b6510a94e2e64a0454f83cd1e9
Nitin Dhar <nitindhar7@yahoo.com>
2011-04-20 02:50:40 -0400

    fixed tweets bg color bug after update floating tweets header bar converted tweet relevance judgement input box to a select field created nav bar. created nav and searchbar stylesheets and made them modular. including all stylesheets in layout. added auto_link helper to convert tweet links to anchor tags

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,11 @@
 class Tweet < ActiveRecord::Base
   belongs_to :query
+  
+  def link_twitter_user(txt)
+    if match = txt.match(/.*?(@)((?:[a-z][a-z]+))(:|\s)/i)
+      user = match[2]
+      txt.gsub!(user, '<a href="http://twitter.com/' + user + '">' + user + '</a>')
+    end
+    txt
+  end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,27 @@
 $(document).ready(function() {
 	
-
+	//floatHeaderBar();
+	
+	$("#searchbar .field").focus();
+	
+	$("#query_tweets h1 input").live('click', function() {
+		var is_checked = $(this).attr("checked");
+		
+		if (is_checked)
+			$('#query_tweets_header').addClass("hoverable");
+		else 
+			$('#query_tweets_header').removeClass("hoverable");
+	});
+	
+	$("#query_tweets_body .query_tweets_row").live('click', function() {
+		$("#query_tweets_body .query_tweets_row").removeClass("active");
+		$(this).addClass("active");
+	});
+	
+	$("#query_main_body .query_main_row").live('click', function() {
+		$("#query_main_body .query_main_row").removeClass("active");
+		$(this).addClass("active");
+	});
 	
 });
 
@@ -9,4 +30,11 @@ function toggleNewQueryForm()
 	$("#new_query_form").slideToggle('fast');
 	$("#new_query_form .field").val('');
 	$("#new_query_form .field:first").focus();
+}
+
+function floatHeaderBar()
+{
+	$(window).scroll(function() {
+		$('#query_tweets_header').css("top", $(window).scrollTop());
+	});
 }
